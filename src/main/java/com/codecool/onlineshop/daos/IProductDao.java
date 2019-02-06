72a305adf11222da721f6b7ad7c81d7931451e0f
Kamil Bednarczyk <genezi333@poczta.fm>
2019-02-06 12:01:24 +0100

    Add quering all products

--- src/main/java/com/codecool/onlineshop/daos/IProductDao.java
+++ src/main/java/com/codecool/onlineshop/daos/IProductDao.java
@@ -1,4 +1,21 @@
 package com.codecool.onlineshop.daos;
 
+import java.util.List;
+
+import com.codecool.onlineshop.containers.Category;
+import com.codecool.onlineshop.models.Product;
+
 public interface IProductDao {
+    List<Product> getAvailableProducts();
+    List<Product> getAllProducts();
+    boolean checkIfAvailable(String productName);
+    Integer getCategoryIdByName(String categoryName);
+    Category getCategoryById(int id);
+    Product getProductById(int id);
+    void updateCategoryName(String oldName, String newName);
+    void addNewCategory(String categoryName);
+    void addNewProduct(List<String> newProductData);
+    void editProduct(Integer productId, List<String> editData);
+    void deactivateProduct(String name);
+    void addProduct(String discount, String productName);
 }
--- src/main/java/com/codecool/onlineshop/daos/ProductDao.java
+++ src/main/java/com/codecool/onlineshop/daos/ProductDao.java
@@ -1,4 +1,53 @@
 package com.codecool.onlineshop.daos;
 
-public class ProductDao {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.codecool.onlineshop.containers.Category;
+import com.codecool.onlineshop.models.Product;
+
+public class ProductDao implements IProductDao {
+
+    DatabaseConnector databaseConnector;
+
+    public ProductDao() {
+        databaseConnector = DatabaseConnector.getInstance();
+    }
+    
+    public List<Product> getAllProducts() {
+        try{
+            String productsQuery = "SELECT * FROM Products;";
+            Statement statement = databaseConnector.c.createStatement();
+    
+            ResultSet results = statement.executeQuery(productsQuery);
+    
+            List<Product> products = new ArrayList<>();
+            while(results.next()){
+                Category category = getCategoryById(results.getInt("category_id"));
+                Product product = new Product(results.getInt("id"),
+                        results.getString("name"),
+                        results.getDouble("price"),
+                        results.getInt("amount"),
+                        Boolean.valueOf(results.getString("available")),
+                        category);
+            }
+
+            return products;
+        } catch (SQLException e) {
+            
+        }    
+    }
+
+    public List<Product> getAvailableProducts() {
+        List<Product> products = getAllProducts();
+        List<Product> availableProducts = products.stream()
+            .filter(p -> p.isAvailable())
+            .collect(Collectors.toList());
+
+        return availableProducts;
+    }
 }
