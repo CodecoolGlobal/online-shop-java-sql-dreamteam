d2277364b36b16339860c74f268164a935e0d5fb
Kamil Bednarczyk <genezi333@poczta.fm>
2019-02-06 12:14:45 +0100

    Implement getting category id by name

--- src/main/java/com/codecool/onlineshop/daos/ProductDao.java
+++ src/main/java/com/codecool/onlineshop/daos/ProductDao.java
@@ -1,5 +1,6 @@
 package com.codecool.onlineshop.daos;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -50,4 +51,22 @@ public class ProductDao implements IProductDao {
 
         return availableProducts;
     }
+
+    public Integer getCategoryIdByName(String name) {
+        try{
+            String productsQuery = "SELECT * FROM Category WHERE name = ?;";
+            PreparedStatement statement = databaseConnector.c.prepareStatement(productsQuery);
+            statement.setString(1, name);
+    
+            ResultSet results = statement.executeQuery();
+            results.next();
+            Integer id = results.getInt("id");
+            if (id.intValue() == 0) {
+                throw new DAOException("Cannot find category ID of such category name");
+            }
+        } catch (SQLException e) {
+            throw new DAOException("Problem occured during querying category ID");
+        }    
+    }
+
 }
