15bd08b42698dd876e120dc8257136224bd1fb8f
PatrykMandrak <patrykmandrak123@gmail.com>
2019-02-06 17:40:19 +0100

    Add delete, edit, add and get all product functions to CustomerService

--- src/main/java/com/codecool/onlineshop/services/CustomerService.java
+++ src/main/java/com/codecool/onlineshop/services/CustomerService.java
@@ -1,4 +1,40 @@
 package com.codecool.onlineshop.services;
 
-public class CustomerService {
+import com.codecool.onlineshop.models.Customer;
+import com.codecool.onlineshop.models.Product;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class CustomerService extends Service {
+    private Customer customer = new Customer("login", "password");
+
+    public void addProduct(Product product, int amount) {
+        Product productToAdd = new Product(product.getId(), product.getName(), product.getPrice(), amount, product.isAvailable(), product.getCategory());
+        customer.getBasket().addProduct(productToAdd);
+    }
+
+    public void deleteProductFromBasket(int productId) {
+        Product product = productDao.getProductById(productId);
+        customer.getBasket().deleteProduct(product);
+    }
+
+    public void editNumberOf(int productId, int amount) {
+        Iterator iterator = customer.getBasket().getIterator();
+
+        while (iterator.hasNext()) {
+            Product product = (Product) iterator.next();
+            if (product.getId() == productId) {
+                product.setAmount(amount);
+            }
+        }
+    }
+
+    public List<Product> getAllProducts() {
+        return productDao.getAvailableProducts();
+    }
+
+    public void placeAnOrder() {
+        //...
+    }
 }
