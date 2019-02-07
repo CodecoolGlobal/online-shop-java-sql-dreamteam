6348adfc47b03def53fae5ec50700c1f224d59ab
Kamil Bednarczyk <genezi333@poczta.fm>
2019-02-07 13:26:06 +0100

    Implement adding product to basket

--- src/main/java/com/codecool/onlineshop/controllers/CustomerController.java
+++ src/main/java/com/codecool/onlineshop/controllers/CustomerController.java
@@ -1,9 +1,12 @@
 package com.codecool.onlineshop.controllers;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import com.codecool.onlineshop.models.Product;
 import com.codecool.onlineshop.services.CustomerService;
+import com.codecool.onlineshop.services.ServiceException;
 import com.codecool.onlineshop.views.MainView;
 
 public class CustomerController {
@@ -28,10 +31,11 @@ public class CustomerController {
                     break;
                 case 2:
                     //Add to busket
-                    printAvailableProducts();
-                    mainView.print("Enter product number");
+                    addProductToBasket();
+                    
                     break;
                 case 3:
+                    editProductQuantity();
                     // Edit product quantity
                     break;
                 case 4:
@@ -62,12 +66,60 @@ public class CustomerController {
         }
     }
 
+    private void addProductToBasket() {
+        List<Product> availableProducts = null;
+        try {
+            availableProducts = service.getAllProducts();
+        } catch (ServiceException e) {
+            mainView.println("Cannot print available products");
+        }
+
+        Iterator<Product> products = availableProducts.iterator();
+        int i = 1;
+        while(products.hasNext()) {
+            mainView.print(i + ". ");
+            mainView.println(products.next().productToString());
+            i++;
+        }
+
+        if (availableProducts == null || availableProducts.isEmpty()) {
+            return;
+        }
+
+        mainView.println("Enter product number: ");
+        int number = mainView.getIntegerInput() - 1;
+        Product product = null;
+        if (number < 0 || number >= availableProducts.size()) {
+            mainView.println("Wrong product number.");
+            mainView.getEmptyInput();
+            return;
+        } else {
+            product = availableProducts.get(number);
+        }
+        mainView.println("Enter amount: ");
+        int amount = mainView.getIntegerInput();
+        if (amount < 1 || amount > product.getAmount()) {
+            mainView.println("Wrong amount.");
+            mainView.getEmptyInput();
+            return;
+        } else {
+            service.addProduct(product, amount);
+            mainView.print("Product added successfuly.");
+            mainView.getEmptyInput();
+        }      
+    }
+
+    private void printAvailableProducts(){
+
+    }
+
     private void printBasket() {
         Iterator busket = service.getBusketIterator();
         int i = 1;
         while(busket.hasNext()) {
             mainView.print(i + ". ");
             mainView.println(((Product) busket.next()).productToString());
+            i++;
         }
     }
 }
